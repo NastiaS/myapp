9d2431af05d7dcd87cd44deeec0840edb5c182ed
NastiaS <anastasiasergienko@gmail.com>
2015-07-11 13:42:45 -0400

    the fake api post generator connected

--- public/javascripts/app/controllers/main.js
+++ public/javascripts/app/controllers/main.js
@@ -1,4 +1,26 @@
-angular.module('ngMyApp', [])
-	.controller('mainController', ['$scope', function($scope){
+var app = angular.module('ngMyApp', []);
+
+	app.controller('mainController', ['$scope', 'PostsFactory', function($scope, PostsFactory){
 		$scope.title = "This is my home page!"
-	}]);
+
+		$scope.loadPosts = function(){
+
+			PostsFactory.bringPosts()
+			.then(function (allPosts) {	 	
+				$scope.posts = allPosts;
+			})			
+		}
+
+	}])
+
+	app.factory('PostsFactory', ['$http', function($http){
+	return {
+		bringPosts: function() {
+		    return $http.get('http://jsonplaceholder.typicode.com/posts')
+		    	.then(function(posts){
+		    		return posts.data
+		    	})
+    	}
+	}
+
+}])
