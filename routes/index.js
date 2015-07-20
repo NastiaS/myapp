60298bda117981ce26bfed20fac8c80ee58f7a87
NastiaS <anastasiasergienko@gmail.com>
2015-07-20 16:41:29 -0400

    ng-route implemented and navigation works

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
+
+router.get('/page/:id', function(req, res, next) {
+  res.render(req.params.id);
+});
 
 module.exports = router;
 
