b96f273e89d74502b6cedcd53fd074961eb54e81
NastiaS <anastasiasergienko@gmail.com>
2015-07-22 17:28:30 -0400

    Delete auth.js

--- server/config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    'googleAuth' : {
-        'clientID'      : '43891219407-dad5aq5j1q6hasnk3rb596frbjp2ocqb.apps.googleusercontent.com',
-        'clientSecret'  : 'Q-vTe3jcZP5BXcIax1C-6lFg',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
-    }
-  
-};
